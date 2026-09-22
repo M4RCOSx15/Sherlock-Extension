8e3343060fb406c24188113c93a29597f409185f
M4RCOSx15 <marcos.gabriel.doth15@gmail.com>
2026-09-22 15:45:53 -0300

    feat: beta inicial funcional - titulo aparece popup

--- popup.js
+++ popup.js
@@ -6,6 +6,21 @@ chrome.tabs.sendMessage(
         action: "BuscarDados"
     },
     (response)=>{
+        const elementoTitulo= document.getElementById("tituloPag");
+        if(chrome.runtime.lastError){
+            console.error("não foi possivel acessar esta aba", chrome.runtime.lastError.message)
+            return;
+        }
+        if(!response){
+         console.log("a aba não retornou nada")
+          return;
+        }
+        if(response && response.title){
+            elementoTitulo.innerText = response.title;
+        }
+        else{
+            elementoTitulo.innerText = "Nenhum Titulo encontrado";
+        }
         console.log(response.title)
     }
 );
